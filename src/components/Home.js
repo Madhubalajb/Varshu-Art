a0a2d1eaf23e07b6278987bd3d639854f47a5e5d
Madhubalajb <jbmadhumng@gmail.com>
2019-05-22 05:10:44 +0530

    Updated site

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,11 @@ class Home extends Component {
 		return (
 			<div className="Home">
 				<center>
-					<h1>Hi... I am J. Jaya Varshini, studying in nineth standard. I love drawing and painting. Here is some of my paintings and drawings :)</h1>
+					<h1><b>Hi... I am J. Jaya Varshini, studying in nineth standard. I love drawing and painting. Here is some of my paintings and drawings :)</b></h1>
 					<button className="btn shadow-sm"><a href="/art">View them</a></button>
+					<p className="fixed-bottom">designed & developed by 
+						<a href="https://madhubalajb.github.io/" target="_blank"> madhubala jayakumaran</a>
+					</p>
 				</center>
 			</div>
 		);
